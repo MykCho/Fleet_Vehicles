91aaae433bb942d693f30e535223554a21170faf
MykCho <137506809+MykCho@users.noreply.github.com>
2024-01-08 01:32:25 +0100

    Add project files.

---
+++ ClassVehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fleet_Vehicles
+{
+    public class Vehicle
+    {
+        public uint Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public double ModelCoef {  get; set; }
+        public uint Year { get; set; }
+        public string Color { get; set; }
+        public double Price {  get; set; } 
+        public string RegNumber {  get; set; }
+        public int Mileage { get; set; }
+        public short  ServiceTime { get; set; }
+
+        public Vehicle(uint id, string brand, string model, double modelCoef, uint year, string color, double price, string regNumber, int mileage, short servicetime) {
+            Id = id;
+            Brand = brand;
+            Model = model;
+            ModelCoef = modelCoef;
+            Year = year;
+            Color = color;
+            Price = price;
+            RegNumber = regNumber;
+            Mileage = mileage;
+            ServiceTime = servicetime;
+
+        
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Fleet_Vehicles
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Vehicle vehicle1 = new Vehicle(1, "Volvo", "Model1", 1.0, 1983, "black", 1000, "AAAA135", 2000, 20);
+
+            Console.WriteLine(vehicle1.Brand);
+
+        }
+    }
+}
