d35ca56b2a02e2c2ff6095676bb9245f8c79c3ea
MykCho <137506809+MykCho@users.noreply.github.com>
2024-01-14 01:37:02 +0100

    Figured how to call correct GetVehicleValue for PassengerVehicle, yay!

--- Program.cs
+++ Program.cs
@@ -4,26 +4,28 @@
     {
         static void Main()
         {
-            var vehicle1 = new Vehicle(1, "Volvo", "VolvoModel1", 1.0, 1983, "black", 1000, "AAAA135", 2000, 20);
-            var vehicle2 = new Vehicle(2, "Toyota", "ToyotaModel1", 1.0, 1995, "white", 1000, "BBBB246", 1500, 1100);
+            List<Vehicle> Fleet = new List<Vehicle>(); //that's our main fleet for now
 
-            List<Vehicle> Fleet = new List<Vehicle>();
+            var vehicle1 = new CargoVehicle(1, "Volvo", "VolvoModel1", 1.0, 2020, "black", 5000, "AAAA135", 2000, 20, 150);
+            var vehicle2 = new PassengerVehicle(2, "Toyota", "ToyotaModel1", 1.0, 2020, "white", 5000, "BBBB246", 250, 1100, 100);
+            var vehicle3 = new CargoVehicle(3, "MAC", "MACModel1", 1.0, 2020, "red", 5000, "CCCC782", 250, 3, 320);
+            var vehicle4 = new PassengerVehicle(4, "MiniCooper", "MCModel1", 1.0, 2020, "beige", 5000, "DDDD197", 500, 3, 10);
+
             Fleet.Add(vehicle1);
             Fleet.Add(vehicle2);
+            Fleet.Add(vehicle3);
+            Fleet.Add(vehicle4);
 
             foreach (var vehicle in Fleet)
             {
-                Console.WriteLine(vehicle.Year);
+                //Console.WriteLine($"{vehicle.Id}, {vehicle.Brand}, Initial value: {vehicle.Price}, Current Value: {vehicle.GetVehicleValue()}");
+                Console.WriteLine($"{(vehicle is CargoVehicle ? "Cargo" : "Passenger")}, {vehicle.Brand}: ${vehicle.Price}/{(vehicle is PassengerVehicle ? (vehicle as PassengerVehicle).GetVehicleValue() : vehicle.GetVehicleValue())}");
+                
+
             }
 
-            var vehicle3 = new CargoVehicle(3, "MAC", "MACModel1", 1.0, 2020, "red", 1530, "CCCC782", 500, 3, 320);
-            Console.WriteLine($"veh3 Initial value: {vehicle3.Price}");
-            Console.WriteLine($"veh3 Current value: {vehicle3.GetVehicleValue()}");
-
-            var vehicle4 = new PassengerVehicle(4, "MiniCooper", "MCModel1", 1.0, 2020, "beige", 1530, "DDDD197", 500, 3, 10);
-            Console.WriteLine($"veh4 Initial value: {vehicle4.Price}");
-            Console.WriteLine($"veh4 Current value: {vehicle4.GetVehicleValue()}");
-
+            Console.WriteLine(Fleet.ElementAt(3).Id);
+            Console.WriteLine((Fleet.ElementAt(3) as PassengerVehicle).GetVehicleValue()); //holy fudge! it works!
         }
     }
 }
